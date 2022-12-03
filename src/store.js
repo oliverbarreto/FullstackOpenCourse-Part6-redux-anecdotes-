81d045bd0cb1888e2aa1486b07a3d908342c87e4
Oliver Barreto <oliver.barreto.online@gmail.com>
2022-12-03 23:17:43 +0000

    Finished: "6.9 Better anecdotes, step7" 1) Installed Redux Toolkit for the project. 2) Already had moved the Redux store creation into its own file store.js 3) Now use Redux Toolkit's configureStore to create the store. 4) Installed Redux DevTools to debug the application's state easier - set up DevTools options in configureStore

--- src/store.js
+++ src/store.js
@@ -1,7 +1,12 @@
-import { createStore } from "redux"
+import { configureStore } from "@reduxjs/toolkit"
 import anecdoteReducer from "./reducers/anecdoteReducer"
 import { composeWithDevTools } from "redux-devtools-extension"
 
-const store = createStore(anecdoteReducer, composeWithDevTools())
+const store = configureStore(
+  {
+    reducer: anecdoteReducer,
+  },
+  composeWithDevTools()
+)
 
 export default store
