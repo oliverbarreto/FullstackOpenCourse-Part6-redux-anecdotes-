51394f68ad103c59e7ebf40d2714c0c458e43b92
Oliver Barreto <oliver.barreto.online@gmail.com>
2022-12-05 18:45:16 +0000

    Finished "6.11 Better anecdotes, step9" 1) Now the application uses the Notification component to display a message for five seconds when the user votes for an anecdote or creates a new anecdote

--- src/components/AnecdotesList.js
+++ src/components/AnecdotesList.js
@@ -1,12 +1,20 @@
 import React from "react"
 import { useSelector, useDispatch } from "react-redux"
 import { voteAnecdote } from "../store/slices/anecdotesSlice"
+import {
+  showNotification,
+  hideNotification,
+} from "../store/slices/notificationsSlice"
 
 const Anecdote = ({ anecdote }) => {
   const dispatch = useDispatch()
 
   const handleVoteAnecdote = () => {
     dispatch(voteAnecdote(anecdote.id))
+    dispatch(showNotification(`You voted for: "${anecdote.content}"`))
+    setTimeout(() => {
+      dispatch(hideNotification())
+    }, 5000)
   }
 
   return (
